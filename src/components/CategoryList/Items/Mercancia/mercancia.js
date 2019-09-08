085298451e5a6fd19bf2ac8e586f8c5d7fe9548c
Cristhian Ferreira <cferreirasuazo@gmail.com>
2019-09-07 21:46:11 -0400

    Agregando form

--- src/components/CategoryList/Items/Mercancia/mercancia.js
+++ src/components/CategoryList/Items/Mercancia/mercancia.js
@@ -2,6 +2,10 @@ import React from "react";
 import styled from "styled-components";
 import Button from '@material-ui/core/Button';
 import Box from '@material-ui/core/Box';
+import Container from '@material-ui/core/Container';
+import {InputLabel, MenuItem, 
+         FormControl, Select,InputBase} from "@material-ui/core"
+
 
 const lista = ["Chair","Bacon","Computer","Chicken","Soap","Mouse","Tuna","Car","Shoes","Table","Hat","Keyboard"]
 const ordenar = ["A-Z","Z-A","Menor-Mayor","Mayor-Menor"]
@@ -9,19 +13,15 @@ const ordenar = ["A-Z","Z-A","Menor-Mayor","Mayor-Menor"]
 
 function Mercancia(){
     return (
-        <div>
-            <h1>Productos y Ofertas</h1>
-            <Box>
-                <p>Buscar</p>
-                <Button>Todo</Button>
-                <div>{lista}</div>
-            </Box>
-
-            <Box>
-                <p>Ordenar por</p>
-                <Button>Destacados</Button>
-            </Box>
-        </div>
+       <Container>
+           <Box>
+               <form>
+                   <FormControl>
+                       <InputLabel htmlFor="custome-input" />
+                   </FormControl>
+               </form>
+           </Box> 
+       </Container>
     )
 }
 

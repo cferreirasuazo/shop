4c745c46bfc973a5ef41b01461584021613a9eb4
Cristhian Ferreira <cferreirasuazo@gmail.com>
2019-09-10 22:27:13 -0400

    On branch mercancia  Your branch is up to date with 'origin/mercancia'.  Changes to be committed:
     Add styles.css
     add 2 select items for search

--- src/components/CategoryList/Items/Mercancia/mercancia.js
+++ src/components/CategoryList/Items/Mercancia/mercancia.js
@@ -1,28 +1,77 @@
+//CREAR funcion para genrar Grid utilizando ese componente
+//Tomar 3-4 items para hacer una fila y luego insertarlo como en 
+//el ejemplo https://material-ui.com/components/grid/
+
 import React from "react";
 import styled from "styled-components";
-import Button from '@material-ui/core/Button';
-import Box from '@material-ui/core/Box';
-import Container from '@material-ui/core/Container';
-import {InputLabel, MenuItem, 
-         FormControl, Select,InputBase} from "@material-ui/core"
+import {Box,Button,Container, InputLabel, MenuItem, 
+         FormControl, Select,InputBase, TextField, Card } from "@material-ui/core"
 
+import "./styles.css";
 
-const lista = ["Chair","Bacon","Computer","Chicken","Soap","Mouse","Tuna","Car","Shoes","Table","Hat","Keyboard"]
-const ordenar = ["A-Z","Z-A","Menor-Mayor","Mayor-Menor"]
+const lista = ["Chair","Bacon","Computer","Chicken","Soap"].map((item)=>{
+    return(
+        <MenuItem value={item}>{item}</MenuItem>
+    )
+})
+const ordenar = ["A-Z","Z-A","Menor-Mayor","Mayor-Menor"].map((item)=>{
+    return(
+        <MenuItem value={item}>{item}</MenuItem>
+    )
+})
 
+const items = ['Lorem',
+               'ipsum',
+               'sed',
+               'vitae',
+               'placerat',
+               'aliquet',
+               'consequat' ].map((item)=>{
+                    return(
+                        <Card>{item}</Card>
+                    )
+               });
 
 function Mercancia(){
     return (
-       <Container>
+       <Container className={"wrapper"}>
+           <Box className="buscador">               
            <Box>
-               <form>
-                   <FormControl>
-                       <InputLabel htmlFor="custome-input" />
-                   </FormControl>
+               <h1 className={""} >Mercancia disponible</h1>
+           </Box>
+           <Box>
+               <form className={"form"}>
+               <FormControl className={"select"}>
+                <InputLabel  htmlFor="categorias">Categorias</InputLabel>
+                    <Select>
+                       {lista}
+                    </Select>
+               </FormControl>
+
+               <FormControl className={"select"}>
+                <InputLabel  htmlFor="ordernar">Ordenar Por</InputLabel>
+                    <Select>
+                       {ordenar}
+                    </Select>
+               </FormControl>
                </form>
-           </Box> 
+            </Box> 
+            </Box>
+           <Box className={"content"}>
+                {items}
+           </Box>
+        
        </Container>
     )
 }
 
+
+
+
+
+
 export default Mercancia
+
+
+
+

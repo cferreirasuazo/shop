6bf4cb81206780b46634ef81f48e1ba46f134e88
Cristhian Ferreira <cferreirasuazo@gmail.com>
2019-09-17 21:02:03 -0400

    On branch categoria  Changes to be committed: 	modified:   src/components/Articulo/Articulo.js 	modified:   src/components/Articulo/styles.css

--- src/components/Articulo/Articulo.js
+++ src/components/Articulo/Articulo.js
@@ -9,48 +9,22 @@ import { makeStyles } from '@material-ui/core/styles';
 import "./styles.css";
 import Grid from '@material-ui/core/Grid';
 
-const useStyles = makeStyles({
-    card: {
-      width: 175,
-      height:175,
-      backgroundColor:"green",
-      
-    },
-    bullet: {
-      display: 'inline-block',
-      margin: '0 2px',
-      transform: 'scale(0.8)',
-    },
-    title: {
-      fontSize: 14,
-    },
-    pos: {
-      marginBottom: 12,
-    },
 
-    grid:{
-      
-    }
-
-
-  });
     
     function Articulo(){
         let articulos = ['Lorem','ipsum',
         'sed', 'vitae', 'placerat',
         'aliquet', 'consequat', 'ut'];
 
-        const classes = useStyles();
-
         let cards = articulos.map((articulo)=>{
             return (
-               <Grid><Card className={classes.card}>{articulo}</Card></Grid>
+               <Grid><Card>{articulo}</Card></Grid>
             )
         })
 
         return(
-            <Container classes={{root:"root"}} maxWidth="lg">
-                <Grid className={"grid"} container alignItems="center">
+            <Container maxWidth="lg">
+                <Grid className={"grid"} container alignItems="">
                 {
                     cards
                 }
